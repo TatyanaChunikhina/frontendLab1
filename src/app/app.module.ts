d8efb347389af3c5ebeb711d426a73bf9fdfca05
Tanya <tchunihina1773t@gmail.com>
2019-12-03 16:23:14 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TextFieldComponent } from './text-field/text-field.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TextFieldComponent,
+    
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, TextFieldComponent]
+})
+export class AppModule { }
